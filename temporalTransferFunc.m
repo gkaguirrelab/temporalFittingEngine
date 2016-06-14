38a46c0b2965006f2781f2cfd0528fb45d973d32
benchinny <ben.chinny@gmail.com>
2016-06-14 16:11:38 -0400

    tuf function partially done

--- temporalTransferFunc.m
+++ temporalTransferFunc.m
@@ -1,10 +1,11 @@
 %%
 
-% PLOTS TIME SERIES AND STIMULUS TOGETHER AS FUNCTIONS. SPECIFY THE SUBJECT 
-% AND DATE BELOW. THE dirPathStim AND dirPathTimeSeries VARIABLES WILL ALSO NEED
-% TO BE CHANGED TO RUN THIS ON CLUSTER. MAKES PLOTS ONE BY ONE--PRESS ANY 
-% KEY TO CYCLE THROUGH THEM
+% CREATES TEMPORAL TRANSFER FUNCTIONS. CODE STILL HAS SOME NUMERIC ISSUES.
+% ATTENTION TASK NOT YET ACCOUNTED FOR. 
 
+% SPECIFY THE SUBJECT AND DATE BELOW. 
+
+% MAKES PLOTS ONE BY ONE--PRESS ANY KEY TO CYCLE THROUGH THEM
 
 %% Identify the user
  if isunix
@@ -18,18 +19,25 @@ elseif ispc
 
 %% SPECIFY SUBJECT AND SESSION, AND DROPBOX FOLDER
 
-subj_name = 'HERO_gka1';
+subj_name = 'HERO_asb1';
 %     'HERO_asb1' 
 %     'HERO_gka1'
 
 
-session = '041516';
+session = '041416';
 %     '041416' ...
 %     '041516' ...
 
 % PATH TO LOCAL DROPBOX
 localDropboxDir = ['/Users/',strtrim(user_name),'/Dropbox-Aguirre-Brainard-Lab/'];
 
+%% HRF PARAMETERS (GRABBED FROM WINAWER MODEL CODE)
+
+param = struct;
+% parameters of the double-gamma hemodynamic filter (HRF)
+param.gamma1 = 6;   % positive gamma parameter (roughly, time-to-peak in seconds)
+param.gamma2 = 12;  % negative gamma parameter (roughly, time-to-peak in seconds)
+param.gammaScale = 10; % scaling factor between the positive and negative gamma componenets
         
 %%
 
@@ -204,9 +212,9 @@ for i = 1:length(folderNameCell)
    % GET ALL THEIR CONTENTS
    runFiles = dir(currentDirPath);
    
-   timeValuesMat = [];
-   
-   stimValuesMat = [];
+   % INITIALIZE MATRICES FOR STORING START TIMES AND STIMULUS VALUES
+   startTimesMat = [];
+   stimValuesMat =[];
    
    % LOOP OVER FILES IN EACH FOLDER
    for j = 1:length(runFiles)
@@ -228,26 +236,13 @@ for i = 1:length(folderNameCell)
               freqValueNum = str2num(freqValueTxt(2));            
           end
           
-          % INITIALIZE MATRICES FOR STORING THE ACTUAL DATA POINTS
-          timeValues = [];
+          % GRAB ALL VALUES IN THE FIRST COLUMN: THESE ARE STARTING TIMES
+          curTimeValue = stimFile(:,1);
+          % COLLECT ALL START TIMES AND THEIR CORRESPONDING STIMULUS VALUES
+          startTimesMat(length(startTimesMat)+1:length(startTimesMat)+length(curTimeValue)) = curTimeValue;
+          stimValuesMat(length(stimValuesMat)+1:length(stimValuesMat)+length(curTimeValue)) = freqValueNum;         
           
-          stimValues = [];
-          
-         % LOOP OVER ENTRIES IN EACH FILE 
-         for k = 1:size(stimFile,1)
-            % ALL THE STIMULUS STARTING TIMES ARE IN THE FIRST COLUMN
-            curTimeValue = stimFile(k,1);
-            % CREATE DATA POINTS--BOX-LIKE FUNCTION
-            timeValues = [timeValues [curTimeValue curTimeValue+1e-10 ...
-                          curTimeValue+stimTime curTimeValue+stimTime+1e-10]]; 
-            stimValues = [stimValues [0 freqValueNum freqValueNum 0]];
-         end
-          
-          % STORE
-          timeValuesMat = [timeValuesMat timeValues];       
-          stimValuesMat = [stimValuesMat stimValues];  
-          
-          % IF THE FILE CONTAINS ATTENTION TASK DATA
+          % IF THE FILE CONTAINS ATTENTION TASK DATA * NEED TO DEAL WITH THIS DATA PROPERLY *
        elseif length(curFile)>20 & strcmp(curFile(length(curFile)-16:length(curFile)),'attentionTask.txt')
            attnFile = load([currentDirPath '/' curFile]); 
            
@@ -264,22 +259,97 @@ for i = 1:length(folderNameCell)
        
    end
    
+   % SORT THE BIG VECTOR OF START TIMES
+   [startTimesMatSorted, stmsInd] = sort(startTimesMat);
+   % SORT THE CORRESPONDING STIMULUS VALUES
+   stimValuesMatSorted = stimValuesMat(stmsInd);
+   
+   % INITIALIZE MATRICES FOR STORING ACTUAL STIMULUS PLOTS
+   timeValuesMatFinal = [];
+   stimValuesMatFinal = [];
+   
+   % LOOP OVER ALL THE START TIMES
+   for j = 1:length(startTimesMatSorted)
+       % GRAB EACH INDIVIDUAL START TIME
+       curTimeValueFinal = startTimesMatSorted(j);
+       curStimValueFinal = stimValuesMatSorted(j);
+       % MAKE 'BOX'--ADD TINY OFFSET TO MAKE SURE INTERPOLATION WORKS
+       % PROPERLY
+       timeValues = [curTimeValueFinal curTimeValueFinal+1e-7 ...
+                    curTimeValueFinal+stimTime-1e-3 curTimeValueFinal-1e-7]; 
+       stimValues = [0 curStimValueFinal curStimValueFinal 0];
+       % STICK IN MATRICES DEFINED BEFORE THE LOOP
+       timeValuesMatFinal = [timeValuesMatFinal timeValues];
+       stimValuesMatFinal = [stimValuesMatFinal stimValues];
+   end
+   
+   % MAKE SURE PLOT STARTS AT 0--SO CONVOLUTION WON'T RETURN NANS
+   if timeValuesMatFinal(1) ~= 0
+      stimValuesMatFinal = [0 stimValuesMatFinal];
+      timeValuesMatFinal = [0 timeValuesMatFinal];
+   end
+   
+   % ALL POSSIBLE STIMULI (SANS ATTENTION TASK)
+   stimHz = [0 2 4 8 16 32 64];
+   
+   % INITIALIZE MATRIX OF COVARIATES
+   regMatrix = [];
+   
+   % MAKE HRF (TAKEN FROM GEOFF'S WINAWER MODEL CODE)
+   % TOTAL DURATION IS SIMPLY LARGEST TIME VALUE
+   modelDuration=max(timeValuesMatFinal);
+   % I HAVE INFERRED THAT THE TEMPORAL RESOLUTION IS 1 FROM THE TIME SERIES, 
+   % BUT IS IT ACTUALLY 0.8?
+   modelResolution=1; 
+   % TIME SAMPLES TO INTERPOLATE
+   t = linspace(1,modelDuration,modelDuration.*modelResolution);
+   % DOUBLE GAMMA HRF
+    BOLDHRF = gampdf(t, param.gamma1, 1) - ...
+    gampdf(t, param.gamma2, 1)/param.gammaScale;
+    % scale to unit sum to preserve amplitude of y following convolution
+    BOLDHRF = BOLDHRF/sum(BOLDHRF);
+    
+   % LOOP OVER POSSIBLE STIMULUS VALUES 
+   for j = 1:length(stimHz)
+      % GET ALL INSIXWA WITH A GIVEN STIMULUS VALUE
+      stimPositions = stimValuesMatFinal == stimHz(j); 
+      stimPositions = double(stimPositions);
+      % SAMPLE AT POINTS t
+      stimulusCovariate = interp1(timeValuesMatFinal,stimPositions,t);
+      % CONVOLVE STIMULUS WITH HRF TO GET REGRESSOR
+      regressor = conv(stimulusCovariate,BOLDHRF);
+      % CUT OFF THE EXTRA CONV VALUES--NEED TO LOOK MORE INTO THIS. CONV IS
+      % WEIRD IN MATLAB
+      regressor = regressor(1:length(stimulusCovariate));
+      % STORE THE REGRESSOR
+      regMatrix(:,j) = regressor';
+      % STORE THE COVARIATES
+      SCmat(:,j) = stimulusCovariate;
+   end
+   
+   % OBTAIN BETA WEIGHTS AND PLOT
+   betaWeights = regMatrix\LHtsMat(i,:)';
+   
    figure;
-   set(gcf,'Position',[439 222 1029 876]);
-   subplot(3,1,3);
-   plot(timeValuesMat,stimValuesMat); hold on
-   plot(attnTimeValues,attnStimValues);
-   xlabel('Time(s)'); ylabel('Stimulus frequency (Hz)');
-   title('Stimulus');
-   set(gca,'FontSize',15);
-   subplot(3,1,2);
-   plot(LHtsMat(i,:));
-   title('Left hemisphere BOLD response');
-   set(gca,'FontSize',15);
-   subplot(3,1,1);
-   plot(RHtsMat(i,:));
-   title('Right hemisphere BOLD response');
-   set(gca,'FontSize',15);
+   plot(stimHz,betaWeights);
    pause;
    close;
+%    figure;
+%    set(gcf,'Position',[439 222 1029 876]);
+%    subplot(3,1,3);
+%    plot(timeValuesMatFinal,stimValuesMatFinal); hold on
+%    plot(attnTimeValues,attnStimValues);
+%    xlabel('Time(s)'); ylabel('Stimulus frequency (Hz)');
+%    title('Stimulus');
+%    set(gca,'FontSize',15);
+%    subplot(3,1,2);
+%    plot(LHtsMat(i,:));
+%    title('Left hemisphere BOLD response');
+%    set(gca,'FontSize',15);
+%    subplot(3,1,1);
+%    plot(RHtsMat(i,:));
+%    title('Right hemisphere BOLD response');
+%    set(gca,'FontSize',15);
+%    pause;
+%    close;
 end
