2fb472f48197e22f7850b60609ad6f7f1c587f08
David Brainard <brainard@psych.upenn.edu>
2016-07-20 14:35:37 -0400

    Refactored with Ben.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- xColorModel/toolbox/@tmriModel/tmriModel.m
+++ xColorModel/toolbox/@tmriModel/tmriModel.m
@@ -4,53 +4,25 @@ classdef tmriModel < handle
 % 6/26/16  dhb  Started in on this
 
     % Public read/write properties
-    properties
-        % Structure of parameters for the current simulated model. The
-        % user can set these and then get the simulated model response.
-        simulateParams = [];
-        
-        % Timebase on which to compute model predictions, specified in
-        % seconds.
-        timebase = [];
-        
-        % Stimulus.  The exact form this takes is model dependent, but it
-        % should allow the compute methods to predict the stimulus on the
-        % timebase, given the parameters of the model.
-        stimulus = [];
-            
-        % HRF
-        boldHRF = [];
-  
+    properties              
     end
     
     % Dependent properties, computed from other parameters
     properties
-        % Neural response on timebase
-        simulateNeuralResponse = [];
-        fitNeuralResponse = [];
-        
-        % Bold response
-        simulateBoldResonse = [];
-        fitBoldResponse = [];
     end
         
     % Public, read-only properties.  These can be set by methods of the
     % parent class (that is, this class) but not by methods of subclasses.
     properties (SetAccess = private, GetAccess = public)  
-
     end
     
     % Protected properties; Methods of the parent class and all of its
     % subclasses can set these.
     properties (SetAccess = protected, GetAccess = public)
-        % Structure of parameters for the current fit.
-        fitParams = [];
-
     end
     
     % Private properties. Only methods of the parent class can set or read these
-    properties (Access = private)
-        
+    properties (Access = private)      
     end
     
     % Public methods
@@ -65,25 +37,22 @@ classdef tmriModel < handle
     % If a subclass does not implement each and every of these methods
     % it cannot instantiate objects.
     methods (Abstract, Access=public)
-        % Default parameters
-        defaultParams(obj,varargin);
+        % Default parameters and bounds in structure form
+        [params,paramsLb,paramsUb] = defaultParams(obj,varargin);
         
         % Print parameters
-        printParams(obj,varagin);
+        print(obj,params,varagin);
 
         % Convert parameter struct to a vector to be used by search
         % routines.
-        x = paramsToVec(obj);
+        x = paramsToVec(obj,params);
         
         % Take the vector and put it back into the object's parameter
         % structure.
-        vecToParams(obj,x)
+        params = vecToParams(obj,x)
         
         % Compute forward simulation of the implemented model, given the parameters
-        computeNeural(obj,varargin);
-          
-        % Might want the object to know how to plot itself.
-        %tmriPlot(obj, sensor);
+        response = computeResponse(obj,params,timebase,stimulus,varargin);
     end
     
     % Methods may be called by the subclasses, but are otherwise private 
