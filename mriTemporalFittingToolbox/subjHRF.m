7c4294b9bb860edd95580ad78c66f1baec54262a
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-08-31 17:12:25 -0400

    subjHRF - updates

--- mriTemporalFittingToolbox/subjHRF.m
+++ mriTemporalFittingToolbox/subjHRF.m
@@ -1,14 +1,27 @@
-function [HRF] = subjHRF(subjDir,nameROI,func)
+function [HRF] = subjHRF(subjDir,roiType,func)
 
 % Calculates the subject-specific HRF across all sessions within a project
 % directory for a given subject
+%
+%   Usage:
+%   [HRF] = subjHRF(subjDir,roiType,func)
+%
+%   Inputs:
+%   subjDir     - subject directory (e.g. '/data/jag/MELA/MelanopsinMR/HERO_asb1')
+%   roiType     - type of ROI (default = 'V1')
+%   func        - functional volume (default = 'wdrf.tf')
+%
+%   Outputs:
+%   HRF.mean    - mean HRF (1 x N vector, msec resolution)
+%   HRF.sem     - standard error of them mean (1 x vector, msec resolution)
+%   HRF.numRuns - number of runs used to calculate the HRF
 %
 %   Written by Andrew S Bock Aug 2016
 
 %% Set defaults
 % ROI
-if ~exist('nameROI','var') || isempty(nameROI)
-    nameROI                         = 'V1';
+if ~exist('roiType','var') || isempty(roiType)
+    roiType                         = 'V1';
 end
 % functional volume
 if ~exist('func','var') || isempty(func)
@@ -24,6 +37,7 @@ bbregName                           = 'func_bbreg.dat';
 sessDirs = listdir(subjDir,'dirs');
 
 %% Get the mean HRF within each session directory
+allHRF = cell(1,length(sessDirs));
 for k = 1:length(sessDirs)
     % session directory paths
     sessionDir                      = fullfile(subjDir,sessDirs{k});
@@ -34,13 +48,14 @@ for k = 1:length(sessDirs)
     [subjectStr,~]                  = fileparts(sessionDir);
     [projectStr,~]                  = fileparts(subjectStr);
     [~,projectName]                 = fileparts(projectStr);
+    allHRF{k}                       = nan(length(boldDirs),HRFdur);
     progBar = ProgressBar(length(boldDirs),'fooing...');
     for i = 1:length(boldDirs)
         % Load fMRI data
         inFile                      = fullfile(sessionDir,boldDirs{i},[func '.nii.gz']);
         inData                      = load_nifti(inFile);
         % Get the ROI voxels
-        switch nameROI
+        switch roiType
             case {'V1' 'V2V3'}
                 anatFileName        = 'mh.areas.anat.vol.nii.gz';
                 boldOutName         = 'mh.areas.func.vol.nii.gz';
@@ -55,7 +70,7 @@ for k = 1:length(sessDirs)
             ' --targ ' anatFile ' --o ' outFile ...
             ' --reg ' bbregFile ' --inv --nearest']);
         areaData                    = load_nifti(outFile);
-        switch nameROI
+        switch roiType
             case {'V1' 'LGN'}
                 ROI                 = find(abs(areaData.vol)==1);
             case 'V2V3'
@@ -70,7 +85,8 @@ for k = 1:length(sessDirs)
         end
         ROIvals                     = thisVol(ROI,:);
         pscVals                     = convert_to_psc(ROIvals);
-        timeSeries                  = nanmean(pscVals);    
+        % Get the median percent signal change for the ROI
+        timeSeries                  = nanmedian(pscVals);
         % Get stimulus data
         stimulus.metaData           = load(fullfile(matDir,matFiles{i}));
         % Get the attention events
@@ -98,7 +114,27 @@ for k = 1:length(sessDirs)
         end
         eventTimes                  = round(attEvents*1000); % attention events (msec)
         sampT                       = inData.pixdim(5); % TR in msec
-        [allHRF(k,i,:)]             = deriveHRF(timeSeries',eventTimes,sampT,HRFdur,numFreqs);
+        allHRF{k}(i,:)              = deriveHRF(timeSeries',eventTimes,sampT,HRFdur,numFreqs);
         progBar(i);
     end
+end
+%% Save HRF
+ct = 0;
+for i = 1:length(allHRF)
+    for j = 1:size(allHRF{i},1)
+        ct = ct + 1;
+        tmpHRF(ct,:) = allHRF{i}(j,:);
+    end
+end
+HRF.mean                            = nanmean(tmpHRF);
+HRF.sem                             = nanstd(tmpHRF) / sqrt(size(tmpHRF,1));
+HRF.numRuns                         = size(tmpHRF,1);
+for k = 1:length(sessDirs)
+    % session directory paths
+    sessionDir                      = fullfile(subjDir,sessDirs{k});
+    hrfDir                          = fullfile(sessionDir,'HRF');
+    if ~exist(hrfDir,'dir')
+        mkdir(hrfDir);
+    end
+    save(fullfile(hrfDir,[roiType '.mat']),'HRF');
 end
