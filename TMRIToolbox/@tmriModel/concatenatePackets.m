6edca8d4dabeaa1f3106be7a70019acaa735196d
benchinny <ben.chinny@gmail.com>
2016-09-11 17:12:47 -0400

    demo script for new fitting approach
    … sans the actual fit. I have written a new function for concatenating
    packets under the TMRI class, ‘tmri.concatenatePackets’

---
+++ TMRIToolbox/@tmriModel/concatenatePackets.m
@@ -0,0 +1,83 @@
+function packetsConc = concatenatePackets(obj,thePackets)
+
+% function packetsConc = concatenatePackets(packets)
+%
+% takes in a list of packets, and concatenates their stimulus and response
+% timebases, as well as their stimulus and response values
+
+p = inputParser;
+p.addRequired('thePackets',@iscell);
+p.parse(thePackets);
+
+% initialize struct to return
+packetsConc = struct;
+
+% vector for concatenating time series'
+responseValues = [];
+% vector for concatenating stimulus matrices
+stimulusValues = [];
+% response and stimulus time bases
+responseTB = [];
+stimulusTB = [];
+
+% determine the number of events in the packet with the most events
+numEventsStore = [];
+% determine the total length of the run
+lengthRun = 0;
+for i = 1:length(thePackets)
+   numEventsStore(i) = size(thePackets{i}.stimulus.values,1);
+   lengthRun = lengthRun + size(thePackets{i}.stimulus.values,2);
+end
+numEvents = max(numEventsStore);
+%%
+
+% mark the index of the stimulus values
+packetPositionMarker = 1;
+% mark the end of the current stimulus and response timebases
+stimulusTBmarker = 0;
+responseTBmarker = 0;
+% store frequency indices
+theFrequencyIndicesStore = [];
+
+for i = 1:length(thePackets)
+    % concatenate response values
+    responseValues = [responseValues thePackets{i}.response.values];
+    % get the current packet's stimulus values. start by creating a matrix
+    % of 0's that has the number of stimuli in the packet as the first
+    % dimension, and the total length of the concatenated stimulus as the
+    % second
+    stimulusValuesForOnePacket = zeros([size(thePackets{i}.stimulus.values,1) lengthRun]);
+    % stick the current packet's stimulus values in this matrix of zeroes
+    stimulusValuesForOnePacket(1:size(thePackets{i}.stimulus.values,1), ...
+                               packetPositionMarker:packetPositionMarker+ ...
+                               size(thePackets{i}.stimulus.values,2)-1) ...
+                               = thePackets{i}.stimulus.values;
+    % update the index marker of the packet stimulus values
+    packetPositionMarker = packetPositionMarker+size(thePackets{i}.stimulus.values,2);
+    % concatenate stimulus values
+    stimulusValues = [stimulusValues; stimulusValuesForOnePacket];
+    
+    % response timebase for one packet: time-shift by timebase marker
+    responseTBforOnePacket = thePackets{i}.response.timebase+responseTBmarker;
+    % concatenate
+    responseTB = [responseTB responseTBforOnePacket];
+    
+    % do the same for stimulus
+    stimulusTBforOnePacket = thePackets{i}.stimulus.timebase+stimulusTBmarker;
+    stimulusTB = [stimulusTB stimulusTBforOnePacket];
+    
+    % update timebase markers
+    stimulusTBmarker = stimulusTB(length(stimulusTB));
+    responseTBmarker = responseTB(length(responseTB));
+    
+    theFrequencyIndicesStore = [theFrequencyIndicesStore thePackets{i}.stimulus.metaData.params.theFrequencyIndices];
+end
+
+% assign to struct
+packetsConc.stimulus.timebase = stimulusTB;
+packetsConc.stimulus.values = stimulusValues;
+packetsConc.response.timebase = responseTB;
+packetsConc.response.values = responseValues;
+packetsConc.metaData.theFrequencyIndices = theFrequencyIndicesStore;
+
+end
---
+++ TestPrograms/BCDM/tmriBDCMDemoCrossVal.m
@@ -0,0 +1,80 @@
+%% LOAD PACKETS
+
+load('/Users/benchin/Desktop/MELA_data/packets_asb1_041416.mat');
+%% TRY CROSS-VALIDATION FOR JUST THE L-M
+indicesOfInterest = [2 5 8 11];
+
+thePackets = {};
+
+for i = 1:length(indicesOfInterest)
+   aPacket = packets{indicesOfInterest(i)};
+   aPacket.response.values = aPacket.response.values{indicesOfInterest(i)};
+   thePackets{length(thePackets)+1} = aPacket; 
+end
+
+clear packets;
+
+%% Construct the model object
+tmri = tmriBDCM;
+
+%% DOWNSAMPLE STIMULI TO THE DESIRED RESOLUTION
+
+% common factor to downsample by
+timeFactor = 100;
+
+startTime = thePackets{1}.stimulus.timebase(1);
+endTime = thePackets{1}.stimulus.timebase(end);
+nTimeSamples = round(endTime/timeFactor);
+newStimulusTimebase = linspace(startTime,endTime,nTimeSamples+1);
+
+%% DOWNSAMPLE HRF TO THE DESIRED RESOLUTION
+
+startTime = thePackets{1}.HRF.timebase(1);
+endTime = thePackets{1}.HRF.timebase(end);
+nTimeSamples = round(endTime/timeFactor);
+newHRFTimebase = linspace(startTime,endTime,nTimeSamples+1);
+
+%%
+% May want a utility function to convert lists of packets into lists of
+% their substructures, and back.
+for ii = 1:length(thePackets)
+    theStimulusList{ii} = thePackets{ii}.stimulus;
+    theHRFList{ii} = thePackets{ii}.HRF;
+end
+
+resampledStimulusList = tmri.resamplePacketStruct(theStimulusList,newStimulusTimebase);
+resampledHRFList = tmri.resamplePacketStruct(theHRFList,newHRFTimebase);
+
+for ii = 1:length(thePackets)
+    thePackets{ii}.stimulus = resampledStimulusList{ii};
+    theHRFList{ii} = resampledHRFList{ii};
+    thePackets{ii}.HRF = theHRFList{ii};
+end
+
+%%
+% concatenate packets
+packetsConc = tmri.concatenatePackets(thePackets);
+% put HRF back in
+packetsConc.HRF = thePackets{1}.HRF;
+% locking matrix
+paramLockMatrix = createParamLockMatrixVanilla(unique(packetsConc.metaData.theFrequencyIndices) ...
+                                               ,packetsConc.metaData.theFrequencyIndices,2);
+
+defaultParamsInfo.nEvents = size(packetsConc.stimulus.values,1);
+
+%% Set parameters
+%
+% Six parameters define a quadratic form in three dimensions, but
+% we normalize the first to 1 so we only need five numbers here.
+params0 = tmri.defaultParams('DefaultParamsInfo',defaultParamsInfo);
+fprintf('Default model parameters:\n');
+tmri.print(params0);
+
+%% Test paramsToVec and vecToParams
+params1 = params0;
+params1.paramsMainMatrix = rand(size(params1.paramMainMatrix));
+x1 = tmri.paramsToVec(params1);
+params2 = tmri.vecToParams(x1);
+if (any(params1.paramMainMatrix ~= params2.paramMainMatrix))
+    error('vecToParams and paramsToVec do not invert');
+end
