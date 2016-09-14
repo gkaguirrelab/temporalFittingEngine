896f15092a0d34b5caafe78cde8299a235e214fa
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-14 12:47:05 -0400

    Tentative packet testing
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- TMRIToolbox/packets/ispacket.m
+++ TMRIToolbox/packets/ispacket.m
@@ -5,7 +5,7 @@ function out = ispacket(packet)
 %
 % 9/14/16   ms      Wrote it.
 
-if isfield(packet, 'stimulus') & isfield(packet, 'response') & isfield(packet, 'kernel') & length(packet) == 1
+if isfield(packet, 'stimulus') && isfield(packet, 'response') && isfield(packet, 'kernel') && isfield(packet, 'metaData') && length(packet) == 1
     out = true;
 else
     out = false;
