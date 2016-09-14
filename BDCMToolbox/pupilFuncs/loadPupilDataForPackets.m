d89ae07c57fbf3c3cde7119760020e9dcf446fef
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-14 11:38:02 -0400

    Updated the loader function
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- BDCMToolbox/pupilFuncs/loadPupilDataForPackets.m
+++ BDCMToolbox/pupilFuncs/loadPupilDataForPackets.m
@@ -1,9 +1,17 @@
-function values = loadPupilDataForPackets(input_dir, stimulus, metaData, params)
+function [values TimeVectorFine] = loadPupilDataForPackets(params)
 % values = loadPupilDataForPackets(input_dir, stimulus, params)
+
+%% STIMULUS VALUES
+Data_Stimulus = load(params.stimulusFile);
+
+% Extract how many runs are needed
+NTRsExpected = sum(Data_Stimulus.params.trialDuration)/params.TRDurSecs;
+
+TimeVectorFine = 0:1:sum(Data_Stimulus.params.trialDuration)*1000-1;
 
 %%% EYE TRACKING DATA %%%
 % Load the eye tracking data
-Data_LiveTrack = load(input_dir);
+Data_LiveTrack = load(params.responseFile);
 
 % Find cases in which the TTL pulse signal was split over the two
 % samples, and remove the second sample.
@@ -27,7 +35,7 @@ for rr = 1:length(Data_LiveTrack.params.Report)
                 Data_LiveTrack.params.Report(rr).PupilWidth_Ch02];
             
             % Special case
-            if strcmp(metaData.sessionDate, '060616') && strcmp(metaData.subjectName, 'HERO_gka1');
+            if strcmp(params.sessionDate, '060616') && strcmp(params.sessionObserver, 'HERO_gka1');
                 Data_LiveTrack_IsTracked = [Data_LiveTrack_IsTracked Data_LiveTrack.params.Report(rr).PupilTracked ...
                     Data_LiveTrack.params.Report(rr).S2Tracked];
             else
@@ -45,7 +53,7 @@ end
 % Now, we reconstruct the time vector of the data.
 TTLPulseIndices = find(Data_LiveTrack_TTLPulses); FirstTTLPulse = TTLPulseIndices(1);
 TimeVectorLinear = zeros(1, size(Data_LiveTrack_TTLPulses, 2));
-TimeVectorLinear(TTLPulseIndices) = (1:params.NTRsExpected)-1;
+TimeVectorLinear(TTLPulseIndices) = (1:NTRsExpected)-1;
 
 % Replace zeros with NaN
 tmpX = 1:length(TimeVectorLinear);
@@ -55,23 +63,23 @@ TimeVectorLinear(isnan(TimeVectorLinear)) = interp1(tmpX(~isnan(TimeVectorLinear
 
 % Resample the timing to 1 msecs sampling
 Data_LiveTrack_PupilDiameter_FineMasterTime = interp1(TimeVectorLinear*params.TRDurSecs*1000, ...
-    Data_LiveTrack_PupilDiameter, params.TimeVectorFine);
+    Data_LiveTrack_PupilDiameter, TimeVectorFine);
 Data_LiveTrack_IsTracked_FineMasterTime = interp1(TimeVectorLinear*params.TRDurSecs*1000, ...
-    Data_LiveTrack_IsTracked, params.TimeVectorFine, 'nearest'); % Use NN interpolation for the binary tracking state
+    Data_LiveTrack_IsTracked, TimeVectorFine, 'nearest'); % Use NN interpolation for the binary tracking state
 
 % Extract the stimulus timing
 keyPressWhich = [];
 keyPressWhen = [];
-for rr = 1:length(stimulus.metaData.params.responseStruct.events)
-    keyPressWhich = [keyPressWhich stimulus.metaData.params.responseStruct.events(rr).buffer.keyCode];
-    keyPressWhen = [keyPressWhen stimulus.metaData.params.responseStruct.events(rr).buffer.when];
+for rr = 1:length(Data_Stimulus.params.responseStruct.events)
+    keyPressWhich = [keyPressWhich Data_Stimulus.params.responseStruct.events(rr).buffer.keyCode];
+    keyPressWhen = [keyPressWhen Data_Stimulus.params.responseStruct.events(rr).buffer.when];
 end
 
 % Extract only the ts
 keyPressWhen = keyPressWhen(keyPressWhich == 18);
 
 % Tack the first t also in this vector
-stimulus.metaData_TTL = [stimulus.metaData.params.responseStruct.tBlockStart keyPressWhen];
+stimulus.metaData_TTL = [Data_Stimulus.params.responseStruct.tBlockStart keyPressWhen];
 
 % Subtract the absolute time of the first t
 stimulus.metaData_TTL_t0 = stimulus.metaData_TTL(1);
@@ -79,8 +87,8 @@ stimulus.metaData_TTL = stimulus.metaData_TTL-stimulus.metaData_TTL_t0;
 
 % Check that we have as many TRs as we expect
 fprintf('> Expecting <strong>%g</strong> TRs - Found <strong>%g</strong> (LiveTrack) and <strong>%g</strong> (OneLight record).\n', ...
-    params.NTRsExpected, sum(Data_LiveTrack_TTLPulses), length(stimulus.metaData_TTL));
-if (params.NTRsExpected == sum(Data_LiveTrack_TTLPulses)) || (params.NTRsExpected == length(stimulus.metaData_TTL))
+    NTRsExpected, sum(Data_LiveTrack_TTLPulses), length(stimulus.metaData_TTL));
+if (NTRsExpected == sum(Data_LiveTrack_TTLPulses)) || (NTRsExpected == length(stimulus.metaData_TTL))
     fprintf('\t>> Expected number of TRs matches actual number.\n');
 else
     error('\t>> Mismatch between expected and actual number of TRs received.');
@@ -89,7 +97,7 @@ end
 % We now have four variables of interest
 %   Data_LiveTrack_IsTracked_FineMasterTime <- Binary array indicating tracking state
 %   Data_LiveTrack_PupilDiameter_FineMasterTime <- Pupil diameter
-%   params.TimeVectorFine <- Time vector in TR time
+%   TimeVectorFine <- Time vector in TR time
 
 % Remove blinks from the pupil data
 Data_LiveTrack_BlinkIdx = [];
@@ -105,7 +113,7 @@ Data_LiveTrack_BlinkIdx = unique(Data_LiveTrack_BlinkIdx);
 Data_LiveTrack_PupilDiameter_FineMasterTime(Data_LiveTrack_BlinkIdx) = NaN;
 
 % Interpolate the elements
-Data_LiveTrack_PupilDiameter_FineMasterTime(isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)) = interp1(params.TimeVectorFine(~isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)), Data_LiveTrack_PupilDiameter_FineMasterTime(~isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)), params.TimeVectorFine(isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)));
+Data_LiveTrack_PupilDiameter_FineMasterTime(isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)) = interp1(TimeVectorFine(~isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)), Data_LiveTrack_PupilDiameter_FineMasterTime(~isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)), TimeVectorFine(isnan(Data_LiveTrack_PupilDiameter_FineMasterTime)));
 % Get the DC
 Data_LiveTrack_PupilDiameter_FineMasterTime_DC = nanmean(Data_LiveTrack_PupilDiameter_FineMasterTime);
 
@@ -117,8 +125,8 @@ Data_LiveTrack_PupilDiameter_FineMasterTime_MeanCentered = (Data_LiveTrack_Pupil
 NFreqsToFilter = 8; % Number of low frequencies to remove
 X = [];
 for ii = 1:NFreqsToFilter
-    X(2*ii-1,:) = sin(linspace(0, 2*pi*ii, size(stimulus.timebase, 2)));
-    X(2*ii,:) = cos(linspace(0, 2*pi*ii, size(stimulus.timebase, 2)));
+    X(2*ii-1,:) = sin(linspace(0, 2*pi*ii, size(TimeVectorFine, 2)));
+    X(2*ii,:) = cos(linspace(0, 2*pi*ii, size(TimeVectorFine, 2)));
 end
 
 % Filter it
