cbedbd9b17943850f8164b9dcab649c5e7c3b8b2
michaeab <micalan@sas.upenn.edu>
2016-09-18 16:19:59 -0400

    updated fitting

---
+++ PRFToolbox/retSearch.m
@@ -0,0 +1,67 @@
+% retSearch.m
+
+%% set defaults
+dataDir = '/Users/Shared/Matlab/gkaguirrelab/mriTemporalFitting/PRFToolbox/';
+
+%% load the stimulus file
+tmp = load(fullfile(dataDir,'pRFimages.mat'));
+
+%% Binarize the stimulus
+stim = tmp.imagesFull;
+stim(stim ~=128) = 1;
+stim(stim == 128 ) = 0;
+
+%% Downsample the frames 
+framesPerPos = 8;
+start = 1:framesPerPos:size(stim,3);
+stop = start(2)-1:framesPerPos:size(stim,3);
+for i = 1:length(start)
+    downStim(:,:,i) = mean(stim(:,:,start(i):stop(i)),3);
+end
+
+%convert degrees to pixels
+stimSize = 39.2257;
+subjectDist = 106.5;
+screenHgt = 1080;
+DVA = rad2deg(2*atan(stimSize/(2*subjectDist)));
+pxlPerDeg = round(((screenHgt)/(DVA))/2);
+
+X = 0:pxlPerDeg:size(downStim,1);
+Y = 0:pxlPerDeg:size(downStim,2);
+X = X(2:end-1);
+Y = Y(2:end-1);
+sigmaList = 13:13:26*5;
+
+
+
+%% generat a predictions
+count = 1;
+tic 
+for x = 1:length(X)
+    for y = 1:length(Y)
+        for s = 1:length(sigmaList)
+
+            TCcell.TCmat(count,:) = makePredTC(downStim,X(x),Y(y),sigmaList(s));
+            TCcell.params(count,:) = [X(x),Y(y),sigmaList(s)];
+            count = count+1;
+        end
+    end
+end
+toc
+save('TCcell', 'TCcell', '-v7.3')
+% V1tc
+
+for v = 1:size(V1tc,1)
+    r = corr(V1tc(v,:)',TCcell.TCmat');
+    paramPreds(v,:) = TCcell.params(find( r == max(r)),:);
+    
+end
+
+    
+
+
+
+
+
+
+
--- PRFToolbox/runFit.m
+++ PRFToolbox/runFit.m
@@ -1,7 +1,7 @@
 % tests the predictions for the retinotopy model
 
 %% set defaults
-dataDir = '/Users/Shared/Matlab/gkaguirrelab/mriTemporalFitting/TMRIToolbox/pRF';
+dataDir = '/Users/Shared/Matlab/gkaguirrelab/mriTemporalFitting/PRFToolbox/';
 
 %% load the stimulus file
 tmp = load(fullfile(dataDir,'pRFimages.mat'));
@@ -11,9 +11,66 @@ stim = tmp.imagesFull;
 stim(stim ~=128) = 1;
 stim(stim == 128 ) = 0;
 
-%% Make simulated timecourse 
-TC = makePrarmsTC(stim,540,540,10);
+%% Downsample the frames 
+framesPerPos = 8;
+start = 1:framesPerPos:size(stim,3);
+stop = start(2)-1:framesPerPos:size(stim,3);
+for i = 1:length(start)
+    downStim(:,:,i) = mean(stim(:,:,start(i):stop(i)),3);
+end
 
-%% test the fit
-R = @(paramsVec)retCorr(stim,TC,paramsVec);
-fmincon(R,[500, 500, 8],[],[],[],[],[0 0 0],[1080*1.5,1080*1.5,30])
+%% Downsample stim size 
+
+%convert degrees to pixels
+stimSize = 39.2257;
+subjectDist = 106.5;
+screenHgt = 1080;
+DVA = rad2deg(2*atan(stimSize/(2*subjectDist)));
+pxlPerDeg = ((screenHgt)/(DVA))/2;
+resample2deg = round(pxlPerDeg/0.5);
+
+smallStim = imresize(downStim,[resample2deg NaN]);
+smallStim(smallStim >=0.4) = 1;
+smallStim(smallStim<0.4) = 0;
+
+
+%% Make simulated timecourse 
+%TC = makePredTC(smallStim,25,60,1);
+load V1tc.mat;
+tic
+
+for voxel = 1:size(V1tc,1)
+
+    TC = V1tc(voxel,:);
+    %% Grid search for starting params
+ 
+    X = 0:floor(size(smallStim,1)/3):size(smallStim,1);
+    Y = 0:floor(size(smallStim,2)/3):size(smallStim,2);
+    X = X(2:end-1);
+    Y = Y(2:end-1);
+    for x = 1:length(X)
+        for y = 1:length(Y);
+            paramsVec=[X(x),Y(y),5];
+            corrMat(x,y) = retCorr(smallStim,TC,paramsVec);
+        end
+    end
+
+    [a,b] = find(corrMat == min(corrMat(:)));
+    initX = X(a);
+    initY = Y(b);
+
+
+    %% test the fit
+    R = @(paramsVec)retCorr(smallStim,TC,paramsVec);
+    preds(voxel,:) =fmincon(R,[initX, initY, 5],[],[],[],[],[0 0 .4],[size(smallStim,1)*1.5,size(smallStim,2)*1.5,30]);
+
+end
+toc
+finalPreds = preds/2;
+[th ecc] = cart2pol(finalPreds(:,1),finalPreds(:,2));
+th = rad2deg(th);
+results = [ecc,th,finalPreds(:,3)];
+
+
+
+save('results', 'results', '-v7.3')
