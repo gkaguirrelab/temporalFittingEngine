62b9a651bd03974ddee87cdbb7a029687485d70f
David Brainard <brainard@psych.upenn.edu>
2016-09-22 09:05:31 -0400

    Rename to QCM
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Models/QCMModel/@tfeQCM/lockMatrix.m
+++ Models/QCMModel/@tfeQCM/lockMatrix.m
@@ -4,19 +4,19 @@ function paramLockMatrix = lockMatrix(obj,params,stimulus,varargin)
 % Return parameter locking matrix.  Currently a stub.
 %
 % Key/value pairs
-%  'LockType' - What kind of locking to do
-%    'vanilla' - Default sensible locking.
+%  'LockType' - string (default 'none') What kind of locking to do?
+%    'none' - Set lock matrix to empty, no lockig.
 
 %% Parse input.
 p = inputParser;
 p.addRequired('params',@isstruct);
-p.addParameter('LockType','vanilla',@ischar);
+p.addParameter('LockType','none',@ischar);
 p.parse(params,varargin{:});
 params = p.Results.params;
 
 %% Create the lock matrix
 switch (p.Results.LockType)
-    case 'vanilla'
+    case 'none'
         paramLockMatrix = [];
     otherwise
         error('Unknown lock type passed');
