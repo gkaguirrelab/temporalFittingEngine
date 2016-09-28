651326a633f7a14a3c6e637fcb5c82f6d0e0cdd6
David Brainard <brainard@psych.upenn.edu>
2016-09-28 15:02:04 -0400

    Lots of progress
    Still need to get resampling right, when response and kernel are passed on timebases with a different deltaT.  The basics are correct, but I don’t quite have the scaling for overall magnitdue done right.
    
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- temporalFittingEngine/validationsTFE/tfeConvolutionDemo.m
+++ temporalFittingEngine/validationsTFE/tfeConvolutionDemo.m
@@ -1,12 +1,27 @@
-function tfeConvolutionDemo
+function tfeConvolutionDemo(varargin)
 % tfeConvolutionDemo
 %
 % Demonstrate and test the applyKernel method of the TFE object
 %
-% 6/26/16  dhb  Wrote it.
+% Optional key/value pairs
+%  'responseDeltaT' - value (default 1).  Response temporal sampling in msec.
+%  'responseDuration' - value (default 100).  Response duration in msec.
+%  'kernelDeltaT' - value (default 1).  Kernel temporal sampling in msec.
+%  'kernelDuration' - value (default 100).  Kernel duration in msec.
 
-%% Clear and close
-clear; close all;
+%% Parse vargin for options passed here
+p = inputParser;
+p.addParameter('responseDeltaT',1,@isnumeric);
+p.addParameter('responseDuration',100,@isnumeric);
+p.addParameter('kernelDeltaT',1,@isnumeric);
+p.addParameter('kernelDuration',10,@isnumeric);
+p.parse(varargin{:});
+kernelDeltaT = p.Results.kernelDeltaT;
+kernelDuration = p.Results.kernelDuration;
+responseDeltaT = p.Results.responseDeltaT;
+responseDuration = p.Results.responseDuration;
+kernelNonZeroTime = 2;
+kernelValue = 2;
 
 %% Construct a model object
 %
@@ -19,34 +34,42 @@ tfe = tfeQCM('verbosity','none');
 %
 % We start with something very simple, so it is easy
 % to check that the convolution is working right.
-responseDeltaT = 1;
-totalResponseTime = 100;
-responseStruct.timebase = 0:responseDeltaT:totalResponseTime;
+responseStruct.timebase = 0:responseDeltaT:responseDuration;
+if (responseStruct.timebase(end) < responseDuration)
+    responseStruct.timebase(end+1) = responseStruct.timebase(end)+responseDeltaT;
+end
 responseStruct.values = zeros(size(responseStruct.timebase));
-responseNonZeroEntries = [1 round(totalResponseTime/2) totalResponseTime+1];
+responseNonZeroEntries = [1 round(responseDuration/4) round(responseDuration/4)+1 round(responseDuration/4)+2 round(responseDuration/2) responseDuration+1];
 responseStruct.values(responseNonZeroEntries) = 1;
 
 %% Create a kernel struct
 %
 % Again start with something simple
-kernelDeltaT = 1;
-totalKernelTime = 100;
-kernelStruct.timebase = 0:kernelDeltaT:totalKernelTime;
+kernelStruct.timebase = 0:kernelDeltaT:kernelDuration;
+if (kernelStruct.timebase(end) < kernelDuration)
+    kernelStruct.timebase(end+1) = kernelStruct.timebase(end)+kernelDeltaT;
+end
 kernelStruct.values = zeros(size(kernelStruct.timebase));
-kernelNonZeroEntries = [1];
-kernelStruct.values(kernelNonZeroEntries) = 1;
+index = find(kernelStruct.timebase <= kernelNonZeroTime);
+kernelStruct.values(index) = kernelValue;
 
 %% Apply kernel to response
-convResponseStruct = tfe.applyKernel(responseStruct,kernelStruct);
+[convResponseStruct,resampledKernelStruct] = tfe.applyKernel(responseStruct,kernelStruct);
 
 %% Make a plot
 figure; clf;
-subplot(3,1,1); hold on
+subplot(4,1,1); hold on
 plot(responseStruct.timebase,responseStruct.values,'ro','MarkerFaceColor','r','MarkerSize',8);
-subplot(3,1,2); hold on
+xlabel('Time (msec)'); ylabel('Response'); title('Input');
+subplot(4,1,2); hold on
 plot(kernelStruct.timebase,kernelStruct.values,'go','MarkerFaceColor','g','MarkerSize',8);
-subplot(3,1,3); hold on
+xlabel('Time (msec)'); ylabel('Response');  title('Kernel');
+subplot(4,1,3); hold on
+plot(resampledKernelStruct.timebase,resampledKernelStruct.values,'ko','MarkerFaceColor','k','MarkerSize',8);
+xlabel('Time (msec)'); ylabel('Response');  title('Resampled Kernel');
+subplot(4,1,4); hold on
 plot(convResponseStruct.timebase,convResponseStruct.values,'bo','MarkerFaceColor','b','MarkerSize',8);
+xlabel('Time (msec)'); ylabel('Response');  title('Output');
 
 end
 
