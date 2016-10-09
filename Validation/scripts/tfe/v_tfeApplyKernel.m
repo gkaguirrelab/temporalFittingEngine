745059b7f0154c5e034f4c853646740dcfd3681f
David Brainard <brainard@psych.upenn.edu>
2016-10-09 12:31:45 -0400

    Rearrrange validations, add QCM validation
    Also delete redundant “Model” from model dir names.
    
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Validation/scripts/tfe/v_tfeApplyKernel.m
+++ Validation/scripts/tfe/v_tfeApplyKernel.m
@@ -1,7 +1,7 @@
-function varargout = uttb_tfeApplyKernel(varargin)
-% varargout = uttb_tfeApplyKernel(varargin)
+function varargout = v_tfeApplyKernel(varargin)
+% varargout = v_tfeApplyKernel(varargin)
 %
-% Works by running v_tfeApplyKernel with various arguments and comparing
+% Works by running t_tfeApplyKernel with various arguments and comparing
 % results with those stored.
 %
 % Validate applyKernel method of tfe parent class.
@@ -15,17 +15,17 @@ function ValidationFunction(runTimeParams)
     
     %% Basic validation
     UnitTest.validationRecord('SIMPLE_MESSAGE', '***** v_tfeApplyKernel *****');
-    validationData1 = v_tfeApplyKernel('generatePlots',runTimeParams.generatePlots);
+    validationData1 = t_tfeApplyKernel('generatePlots',runTimeParams.generatePlots);
     UnitTest.validationData('validationData1',validationData1);
     
     %% Change kernel timebase deltaT
     UnitTest.validationRecord('SIMPLE_MESSAGE', '***** v_tfeApplyKernel(''kernelDeltaT'',0.5) *****');
-    validationData2 = v_tfeApplyKernel('kernelDeltaT',0.5,'generatePlots',runTimeParams.generatePlots);
+    validationData2 = t_tfeApplyKernel('kernelDeltaT',0.5,'generatePlots',runTimeParams.generatePlots);
     UnitTest.validationData('validationData2',validationData2);
     
     %% Change kernel timebase deltaT to an odd value
     UnitTest.validationRecord('SIMPLE_MESSAGE', '***** v_tfeApplyKernel(''kernelDeltaT'',0.8) *****');
-    validationData3 = v_tfeApplyKernel('kernelDeltaT',0.8,'generatePlots',runTimeParams.generatePlots);
+    validationData3 = t_tfeApplyKernel('kernelDeltaT',0.8,'generatePlots',runTimeParams.generatePlots);
     UnitTest.validationData('validationData3',validationData3);
     
 end
