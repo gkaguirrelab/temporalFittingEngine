5fe4e1b9ca369477d1d6b51267e63dfa28f02c46
gkaguirre <gkaguirre@me.com>
2016-10-18 11:21:58 -0400

    Basic working cross-validation, including demo
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ Library/CrossValidationRoutines/crossValidateDemo.m
@@ -0,0 +1,109 @@
+% crossValidateDemo
+%
+% Demonstrate cross-validation of simulated data using the BTRM model
+%
+
+%% Clear and close
+clear; close all;
+
+%% Construct the model object
+tfeHandle = tfeBTRM('verbosity','none');
+
+%% Temporal domain of the stimulus
+deltaT = 100; % in msecs
+totalTime = 120000; % in msecs
+stimulusStruct.timebase = linspace(0,totalTime-deltaT,totalTime/deltaT);
+nTimeSamples = size(stimulusStruct.timebase,2);
+
+%% Specify the stimulus values.
+% We create here a step function of neural activity, with half-cosine ramps
+%  on and off. There will be ten instances.
+
+nInstances=10;
+defaultParamsInfo.nInstances=nInstances;
+
+for ii=1:nInstances
+    stepDuration=12000; % msecs
+    stepOnset=(ii-1)*stepDuration+100; % msecs
+    rampDuration=3000; % msecs
+    
+% the square wave step
+stimulusStruct.values(ii,:)=zeros(1,nTimeSamples);
+stimulusStruct.values(ii,round(stepOnset/deltaT): ...
+                      round(stepOnset/deltaT)+round(stepDuration/deltaT)-1)=1;
+% half cosine ramp on
+stimulusStruct.values(ii,round(stepOnset/deltaT): ...
+                      round(stepOnset/deltaT)+round(rampDuration/deltaT)-1)= ...
+                      fliplr(cos(linspace(0,pi,round(rampDuration/deltaT))/2));
+% half cosine ramp off
+stimulusStruct.values(ii,round(stepOnset/deltaT)+round(stepDuration/deltaT)-round(rampDuration/deltaT): ...
+                      round(stepOnset/deltaT)+round(stepDuration/deltaT)-1)= ...
+                      cos(linspace(0,pi,round(rampDuration/deltaT))/2);
+                  
+end % loop over instances building the stimulus values
+
+
+%% Define a kernelStruct. In this case, a double gamma HRF
+hrfParams.gamma1 = 6;   % positive gamma parameter (roughly, time-to-peak in secs)
+hrfParams.gamma2 = 12;  % negative gamma parameter (roughly, time-to-peak in secs)
+hrfParams.gammaScale = 10; % scaling factor between the positive and negative gamma componenets
+
+kernelStruct.timebase=stimulusStruct.timebase;
+
+% The timebase is converted to seconds within the function, as the gamma
+% parameters are defined in seconds.
+hrf = gampdf(kernelStruct.timebase/1000, hrfParams.gamma1, 1) - ...
+    gampdf(kernelStruct.timebase/1000, hrfParams.gamma2, 1)/hrfParams.gammaScale;
+kernelStruct.values=hrf;
+
+% prepare this kernelStruct for use in convolution as a BOLD HRF
+kernelStruct=prepareHRFKernel(kernelStruct);
+
+%% Get the default forward model parameters
+params0 = tfeHandle.defaultParams('defaultParamsInfo', defaultParamsInfo);
+params0.noiseSd = 0.05;
+
+%% Create the packetCellArray
+
+% We will now create a set of 10 packets. Each packet will have two
+% stimulus types, each with a different expected amplitude and tau.
+
+stimLabels={'stimA','stimB'};
+stimTypes=[1 2 1 2 1 2 1 2 1 2]';
+stimMeansAmplitude=[1,0.5];
+nPackets=10;
+
+for pp=1:nPackets
+    
+    % ensure that the packet from the pior loop is empty
+    thePacket=[];
+    
+    % start the packet assembly
+    thePacket.stimulus = stimulusStruct;
+    thePacket.kernel = [];%kernelStruct;
+    thePacket.metaData = [];
+    
+    % Randomize the order of the stimTypes
+    ix = randperm(10);
+    thePacket.stimulus.metaData.stimTypes=stimTypes(ix);
+    thePacket.stimulus.metaData.stimLabels=stimLabels;
+    
+    % Create some params to define the simulated data for this packet
+    paramsLocal=params0;
+    for ii=1:nInstances
+        paramsLocal.paramMainMatrix(ii,1)=stimMeansAmplitude(thePacket.stimulus.metaData.stimTypes(ii));
+    end
+    
+    % Generate the simulated response
+    simulatedResponseStruct = tfeHandle.computeResponse(paramsLocal,thePacket.stimulus,[],'AddNoise',true);
+    
+    % Add the simulated response to this packet
+    thePacket.response=simulatedResponseStruct;
+    
+    % Add this packet to the growing cell array
+    packetCellArray{pp}=thePacket;
+end % loop over number of packets to be created
+
+% Conduct the cross validation
+[ trainParamsFit, trainfVals ] = crossValidateFits( packetCellArray, tfeHandle );
+
--- Library/CrossValidationRoutines/crossValidateFits.m
+++ Library/CrossValidationRoutines/crossValidateFits.m
@@ -9,10 +9,10 @@ function [ trainParamsFit, trainfVals ] = crossValidateFits( packetCellArray, tf
 % pairs recognized by the calling routine are not needed here.
 p = inputParser; p.KeepUnmatched = true;
 p.addRequired('packetCellArray',@iscell);
-p.addRequired('tfeHandle',@(x)(strcmp(class(x),'function_handle')));
+p.addRequired('tfeHandle',@(x)(~isempty(x)));
 p.addParameter('defaultParamsInfo',[],@(x)(isempty(x) | isstruct(x)));
 p.addParameter('aggregateMethod','mean',@ischar);
-p.parse(paramsCellArray, tfeHandle, varargin{:});
+p.parse(packetCellArray, tfeHandle, varargin{:});
 
 %% Check the input
 
@@ -26,14 +26,11 @@ end
 
 % check that every packet has the same stimulus labels and the same unique
 % set of stimulus types
-
 uniqueStimTypes=unique(packetCellArray{1}.stimulus.metaData.stimTypes);
 uniqueStimLabels=unique(packetCellArray{1}.stimulus.metaData.stimLabels);
-
 if ~(length(uniqueStimTypes)==length(uniqueStimLabels))
     error('There must be as many unique stimTypes as stimLabels');
 end
-
 for pp=2:nPackets
     if ~isequal(uniqueStimTypes, unique(packetCellArray{pp}.stimulus.metaData.stimTypes))
         error('Not all of the packets have the same stimTypes');
@@ -49,7 +46,7 @@ end
 nPartitions=nPackets;
 testSets=eye(nPackets);
 
-% Empty the variables that aggregate across the partitions.
+% Empty the variables that aggregate across the partitions
 trainParamsFit=[];
 trainfVals=[];
 testfVals=[];
@@ -66,14 +63,19 @@ for pp=1:nPartitions
     % Loop through the trainPackets
     for tt=1:length(trainPackets)
         
+        % report our progress
+        fprintf('* Partition, packet <strong>%g</strong> , <strong>%g</strong>\n', pp, tt);
+        
         % get the number of instances in this packet
-        if isempty(defaultParamsInfo)
+        if isempty(p.Results.defaultParamsInfo)
             defaultParamsInfo.nInstances=size(trainPackets{tt}.stimulus.values,1);
+        else
+            defaultParamsInfo.nInstances=p.Results.defaultParamsInfo.nInstances;
         end
         
         % do the fit
         [paramsFit,fVal,~] = ...
-            tfeHandle.fitResponse(thePacket,...
+            tfeHandle.fitResponse(trainPackets{tt},...
             'defaultParamsInfo', defaultParamsInfo, ...
             varargin{:});
         
@@ -96,19 +98,55 @@ for pp=1:nPartitions
     end % loop over trainPackets
     
     % Aggregate across trainining packets
+    % trainParamsFit has the dimensions: partition x uniqueStimType x param
     switch p.Results.aggregateMethod
         case 'mean'
-            trainParamsFit(pp,:,:)=mean(trainParamsFit,1);
+            trainParamsFit(pp,:,:)=mean(trainParamsFitLocal,1);
             trainfVals(pp)=mean(trainfValsLocal);
         case 'median'
-            trainParamsFit(pp,:,:)=median(trainParamsFit,1);
+            trainParamsFit(pp,:,:)=median(trainParamsFitLocal,1);
             trainfVals(pp)=median(trainfValsLocal);
         otherwise
             error('This is an undefined aggregation method');
     end % switch over aggregation methods
     
-    % Apply the model to the test data
+    % loop over the test set
+    for tt=1:length(testPackets)
+        
+        % get the number of instances in this packet
+        if isempty(p.Results.defaultParamsInfo)
+            defaultParamsInfo.nInstances=size(testPackets{tt}.stimulus.values,1);
+        else
+            defaultParamsInfo.nInstances=p.Results.defaultParamsInfo.nInstances;
+        end
+        
+        % Build a params structure that holds the prediction from the test set
+        predictParams = tfeHandle.defaultParams('defaultParamsInfo', defaultParamsInfo);
+        for ii=1:defaultParamsInfo.nInstances
+            predictParams.paramMainMatrix(ii,:)= trainParamsFit(pp, testPackets{tt}.stimulus.metaData.stimTypes(ii), :);
+        end
+        
+        % create a paramsVec to pass to the fitError method
+        predictParamsVec=tfeHandle.paramsToVec(predictParams);
+        
+        % get the error for the prediction of this test packet
+        [fVal,~] = tfeHandle.fitError(predictParamsVec,testPackets{tt},varargin{:});
+        
+        % save the fVal for this trainPacket fit
+        testfValsLocal(tt)=fVal;
+        
+    end % loop over instances in this test packet
     
+    % Aggregate across test packets
+    switch p.Results.aggregateMethod
+        case 'mean'
+            testfVals(pp)=mean(testfValsLocal);
+        case 'median'
+            testfVals(pp)=median(testfValsLocal);
+        otherwise
+            error('This is an undefined aggregation method');
+    end % switch over aggregation methods
+
 end % loop over partitions
 
 
