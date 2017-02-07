0196d407bd41a2ea25fb1744a022ac1a93f2e02b
gkaguirre <gkaguirre@me.com>
2017-02-07 13:55:03 -0500

    DEDU model modifications
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- Models/DEDU/forwardModelDEDU/forwardModelDEDU.m
+++ Models/DEDU/forwardModelDEDU/forwardModelDEDU.m
@@ -1,17 +1,18 @@
-function [modelResponseStruct] = forwardModelDEDU(params,stimulusStruct)
+function [modelResponseStruct] = forwardModelDEDU(obj,params,stimulusStruct)
 %% forwardModelDEDU
 %
 % This function creates a model of neural response given a vector of
 % stimulus input, a vector of time points, and a set of parameters.
 %
 % The model of neural activity is a step function, controlled by three
-% parameters: amplitude, delay, and duration (secs)
+% parameters: amplitude, delay, and duration (secs). A kernel is built
+% based upon the parameters, and then the stimulus vector is convolved by
+% this kernel.
 %
 
 amplitudeVec=params.paramMainMatrix(:,strcmp(params.paramNameCell,'amplitude'));
 delayVec=params.paramMainMatrix(:,strcmp(params.paramNameCell,'delay'));
 durationVec=params.paramMainMatrix(:,strcmp(params.paramNameCell,'duration'));
-
 
 %% Define basic model features
 
@@ -19,63 +20,33 @@ durationVec=params.paramMainMatrix(:,strcmp(params.paramNameCell,'duration'));
 numInstances=size(stimulusStruct.values,1);
 modelLength = length(stimulusStruct.timebase);
 
-% the model has parameters that are tuned for units of seconds, so
-% we convert our timebase
-timebaseMsecs=stimulusStruct.timebase;
-timebaseSecs=timebaseMsecs/1000;
-
 % pre-allocate the responseMatrix variable here for speed
 responseMatrix=zeros(numInstances,modelLength);
+
+% determine the deltaT of the stimulus and make sure it is regularly
+% sampled (in msecs)
+check = diff(stimulusStruct.timebase);
+deltaT = check(1);
+if (any(abs(check - check(1)) > 1e-6))
+    error('Response structure timebase is not regularly sampled');
+end
 
 %% We loop through each column of the stimulus matrix
 for i=1:numInstances
     
     % grab the current stimulus
-    stimulus=stimulusStruct.values(i,:)';
+    subStimulusStruct=stimulusStruct;
+    subStimulusStruct.values=subStimulusStruct.values(i,:);
 
-    % Find the stimulus onsets so that we can adjust the model to it. We
-    % do that by finding a [0 1] edge from a difference operator.
-    tmp = diff(stimulus');
-    tmp(tmp < 0) = 0;
-    tmp(tmp > 0) = 1;
+    % Build the kernel
+    kernelStruct.timebase=0:deltaT:round(durationVec(i)*1000/deltaT)-1;
+    kernelStruct.values=kernelStruct.timebase*0+amplitudeVec(i);
     
-    % Check if the very first value is 1, in which case the stim onset is
-    % at the initial value
-    if tmp(1)==1
-        stimOnsetIdx=1;
-    else
-        stimOnsetIdx = strfind(tmp, [0 1]);
-    end
-    
-    % If the stimOnsetIdx is empty, check if the first value of the
-    % stimulus vector is different from the last. This tells us that the
-    % stimulus begins at the first timepoint
-    if (isempty(stimOnsetIdx) && ~(stimulus(1)==stimulus(end)))
-        stimOnsetIdx=1;
-    end
-    
-    % If we can't find a stim onset, return an error.
-    if ~length(stimOnsetIdx)==1
-        error('Cannot find a unique stimulus onset for this instance. Assuming start at first time point.')
-    end
-    
-    % Determine the parameters of the step
-    stepOnsetTime=timebaseSecs(stimOnsetIdx)+delayVec(i);
-    tempTimeDiff=abs(timebaseSecs-stepOnsetTime);
-    stepOnsetIdx=find(tempTimeDiff==min(tempTimeDiff));
-    stepOnsetIdx=stepOnsetIdx(1);
-    
-    stepOffsetTime=timebaseSecs(stimOnsetIdx)+delayVec(i)+durationVec(i);
-    tempTimeDiff=abs(timebaseSecs-stepOffsetTime);
-    stepOffsetIdx=find(tempTimeDiff==min(tempTimeDiff));
-    stepOffsetIdx=stepOffsetIdx(1);
-    
-    % Make the step
-    yNeural=timebaseSecs*0;
-    yNeural(stepOnsetIdx:stepOffsetIdx)=amplitudeVec(i);
-    
+    % Convolve the stimulus by the kernel
+    yNeuralStruct=obj.applyKernel(subStimulusStruct,kernelStruct);
+        
     %% Place yNeural into the growing neuralMatrix
-    responseMatrix(i,:)=yNeural;
+    responseMatrix(i,:)=yNeuralStruct.values';
     
 end % loop over rows of the stimulus matrix
 
