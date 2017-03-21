89638ec228513063c85ad5c80749c153f6da5592
gkaguirre <gkaguirre@me.com>
2017-03-20 22:44:30 -0400

    Added capability for colored noise
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- Models/BTRM/@tfeBTRM/computeResponse.m
+++ Models/BTRM/@tfeBTRM/computeResponse.m
@@ -9,7 +9,7 @@ function modelResponseStruct = computeResponse(obj,params,stimulusStruct,kernelS
 %   'AddNoise' - true/false (default false).  Add noise to computed
 %     response? Useful for simulations.
 
-%% Parse input. 
+%% Parse input.
 % Setting 'KeepUmatched' to true means that we can pass the varargin{:})
 % along from a calling routine without an error here, if the key/value
 % pairs recognized by the calling routine are not needed here.
@@ -34,28 +34,15 @@ end
 %% Optionally, convolve with a passed kernel
 modelResponseStruct = obj.applyKernel(modelResponseStruct,kernelStruct,varargin{:});
 
-%% Optional add 1/f^2 noise
+%% Optional add noise
 if p.Results.addNoise
-    if ~isfield(params, 'noiseType')
-        params.noiseType='white';
-        
+    if ~isfield(params, 'noiseInverseFrequencyPower')
+        params.noiseInverseFrequencyPower=0;
     end
-switch params.noiseType
-    case 'white'
-        modelResponseStruct.values = modelResponseStruct.values + normrnd(0,params.noiseSd,size(modelResponseStruct.values));
-    case 'red' % aka 'brown'ian noise or auto-regressive noise
-        noise.timebase=modelResponseStruct.timebase;
-        noise.values=normrnd(0,1,size(modelResponseStruct.values));
-        noiseFilter.timebase=modelResponseStruct.timebase;
-        noiseFilter.values=exp(-1/params.noiseTimeConstant*noiseFilter.timebase);
-        noiseFilter=normalizeKernelArea(noiseFilter);
-        noise=obj.applyKernel(noise,noiseFilter);
-        noise.values=noise.values/std(noise.values)*params.noiseSd;
-        modelResponseStruct.values = modelResponseStruct.values + noise.values;
-    case 'none'
-    otherwise
-        error('That is not a noise type that I know');
-end
+    cn = dsp.ColoredNoise(params.noiseInverseFrequencyPower,length(modelResponseStruct.timebase),1);
+    noise = cn()';
+    noise = noise/std(noise)*params.noiseSd;
+    modelResponseStruct.values = modelResponseStruct.values + noise;
 end
 
 end
