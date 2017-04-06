22798eb34489fd23f4299f3cf06c287bd37ce1dc
gkaguirre <gkaguirre@me.com>
2017-04-06 16:14:35 -0400

    Able to pass vlb and vub to the fitter. Demo of this added to the tutorial.
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- Models/TPUP/parameterDefinitionTPUP/parameterDefinitionTPUP.m
+++ Models/TPUP/parameterDefinitionTPUP/parameterDefinitionTPUP.m
@@ -5,10 +5,16 @@ function paramStruct = parameterDefinitionTPUP(nInstances, varargin)
 % step-function pupil model.
 % This includes default parameters plus lower and upper bounds,
 % as well as a field with parameter names.
-
+%
+% If passed, the paramMainMtrix (which are the initial values), vlb,
+%   or vub will be used instead of the default values.
+%
 %% Parse vargin for options passed here
 p = inputParser; p.KeepUnmatched = true;
 p.addRequired('nInstances',@isnumeric);
+p.addParameter('initialValues',[],@isnumeric);
+p.addParameter('vlb',[],@isnumeric);
+p.addParameter('vub',[],@isnumeric);
 p.parse(nInstances,varargin{:});
 
 % Parameters:
@@ -33,29 +39,46 @@ paramStruct.paramNameCell = {...
     };
 
 % initial values
-paramStruct.paramMainMatrix = [];
-paramStruct.paramMainMatrix(:,1) = 200.*ones([nInstances 1]);
-paramStruct.paramMainMatrix(:,2) = 200.*ones([nInstances 1]);
-paramStruct.paramMainMatrix(:,3) = 10.*ones([nInstances 1]);
-paramStruct.paramMainMatrix(:,4) = -10.*ones([nInstances 1]);
-paramStruct.paramMainMatrix(:,5) = -25.*ones([nInstances 1]);
-paramStruct.paramMainMatrix(:,6) = -25.*ones([nInstances 1]);
+if isempty(p.Results.initialValues)
+    paramStruct.paramMainMatrix(:,1) = 200.*ones([nInstances 1]);
+    paramStruct.paramMainMatrix(:,2) = 200.*ones([nInstances 1]);
+    paramStruct.paramMainMatrix(:,3) = 10.*ones([nInstances 1]);
+    paramStruct.paramMainMatrix(:,4) = -10.*ones([nInstances 1]);
+    paramStruct.paramMainMatrix(:,5) = -25.*ones([nInstances 1]);
+    paramStruct.paramMainMatrix(:,6) = -25.*ones([nInstances 1]);
+else
+    for ii=1:6
+        paramStruct.paramMainMatrix(:,ii) = p.Results.initialValues(ii).*ones([nInstances 1]);
+    end
+end
 
 % set lower bounds
-paramStruct.vlb(:,1) = repmat(0,[nInstances 1]);
-paramStruct.vlb(:,2) = repmat(100,[nInstances 1]);
-paramStruct.vlb(:,3) = repmat(1,[nInstances 1]);
-paramStruct.vlb(:,4) = repmat(-2000,[nInstances 1]);
-paramStruct.vlb(:,5) = repmat(-2000,[nInstances 1]);
-paramStruct.vlb(:,6) = repmat(-2000,[nInstances 1]);
+if isempty(p.Results.vlb)
+    paramStruct.vlb(:,1) = repmat(0,[nInstances 1]);
+    paramStruct.vlb(:,2) = repmat(100,[nInstances 1]);
+    paramStruct.vlb(:,3) = repmat(1,[nInstances 1]);
+    paramStruct.vlb(:,4) = repmat(-2000,[nInstances 1]);
+    paramStruct.vlb(:,5) = repmat(-2000,[nInstances 1]);
+    paramStruct.vlb(:,6) = repmat(-2000,[nInstances 1]);
+else
+    for ii=1:6
+        paramStruct.vlb(:,ii) = p.Results.vlb(ii).*ones([nInstances 1]);
+    end
+end
 
 % set upper bounds
-paramStruct.vub(:,1) = repmat(500,[nInstances 1]);
-paramStruct.vub(:,2) = repmat(350,[nInstances 1]);
-paramStruct.vub(:,3) = repmat(30,[nInstances 1]);
-paramStruct.vub(:,4) = repmat(0,[nInstances 1]);
-paramStruct.vub(:,5) = repmat(0,[nInstances 1]);
-paramStruct.vub(:,6) = repmat(0,[nInstances 1]);
+if isempty(p.Results.vub)
+    paramStruct.vub(:,1) = repmat(500,[nInstances 1]);
+    paramStruct.vub(:,2) = repmat(350,[nInstances 1]);
+    paramStruct.vub(:,3) = repmat(30,[nInstances 1]);
+    paramStruct.vub(:,4) = repmat(0,[nInstances 1]);
+    paramStruct.vub(:,5) = repmat(0,[nInstances 1]);
+    paramStruct.vub(:,6) = repmat(0,[nInstances 1]);
+else
+    for ii=1:6
+        paramStruct.vub(:,ii) = p.Results.vub(ii).*ones([nInstances 1]);
+    end
+end
 
 
 
